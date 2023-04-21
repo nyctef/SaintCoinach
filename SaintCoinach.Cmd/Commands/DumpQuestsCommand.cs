86c51f33904947c31736d009df72ab8d01ca5eb0
nyctef <nyctef@nyctef.com>
2023-04-21 17:31:46 +0100

    trying out a new command: currently just prints a list of msq quests

---
+++ SaintCoinach.Cmd/Commands/DumpQuestsCommand.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tharga.Toolkit.Console;
+using Tharga.Toolkit.Console.Command;
+using Tharga.Toolkit.Console.Command.Base;
+
+using SaintCoinach;
+using SaintCoinach.Ex;
+using SaintCoinach.Text;
+using SaintCoinach.Xiv;
+
+#pragma warning disable CS1998
+
+namespace SaintCoinach.Cmd.Commands {
+    public class DumpQuestsCommand : ActionCommandBase {
+        private ARealmReversed _Realm;
+
+        public DumpQuestsCommand(ARealmReversed realm)
+            : base("msqs", "Dump MSQ quests, quest links and quest text") {
+            _Realm = realm;
+        }
+
+        public override async Task<bool> InvokeAsync(string paramList) {
+
+            var quests = _Realm.GameData.GetSheet<Quest>();
+            var endwalker = quests[70_000];
+            var pending = new Queue<Quest>();
+            var seen = new HashSet<XivString>();
+            pending.Enqueue(endwalker);
+            while (pending.Any())
+            {
+                var next = pending.Dequeue();
+                if (seen.Contains(next.Id))
+                {
+                    continue;
+                }
+                Console.WriteLine($"{next.Id} {next.Name}");
+                seen.Add(next.Id);
+                if (!next.Requirements.PreviousQuest.PreviousQuests.Any())
+                {
+                    Console.WriteLine("^ is a starting quest");
+                }
+                foreach (var prereq in next.Requirements.PreviousQuest.PreviousQuests)
+                {
+                    Console.WriteLine($" > {prereq.Name}");
+                    pending.Enqueue(prereq);
+                }
+            }
+
+            return true;
+        }
+    }
+}
