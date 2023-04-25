e9c1c556b5fa4b21c77c3f8ab64b494b32882d3c
nyctef <nyctef@nyctef.com>
2023-04-25 09:22:54 +0100

    rework this command to dump all quest text

--- SaintCoinach.Cmd/Commands/DumpAllQuestsCommand.cs
+++ SaintCoinach.Cmd/Commands/DumpAllQuestsCommand.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Newtonsoft.Json;
 using Tharga.Toolkit.Console;
 using Tharga.Toolkit.Console.Command;
@@ -13,16 +14,15 @@ using SaintCoinach;
 using SaintCoinach.Ex;
 using SaintCoinach.Text;
 using SaintCoinach.Xiv;
-using SaintCoinach.Xiv.Collections;
 
 #pragma warning disable CS1998
 
 namespace SaintCoinach.Cmd.Commands {
-    public class DumpSingleQuestCommand : ActionCommandBase {
+    public class DumpAllQuestsCommand : ActionCommandBase {
         private ARealmReversed _Realm;
 
-        public DumpSingleQuestCommand(ARealmReversed realm)
-            : base("dsq", "Dump a named quest") {
+        public DumpAllQuestsCommand(ARealmReversed realm)
+            : base("allq", "Dump text and prereqs for all quests") {
             _Realm = realm;
         }
 
@@ -30,6 +30,8 @@ namespace SaintCoinach.Cmd.Commands {
         {
             public int Key;
             public string InternalId;
+            public List<int> PreviousQuests;
+            public List<int> NextQuests;
             public List<string> TodosEn;
             public List<string> JournalEn;
             public List<DialogLine> DialogueEn;
@@ -48,25 +50,61 @@ namespace SaintCoinach.Cmd.Commands {
         
         public override async Task<bool> InvokeAsync(string paramList) {
 
-            if (!int.TryParse(paramList, out var questId))
-            {
-                throw new Exception($"a quest id is required (failed to parse [{paramList}])");
-            }
-
             // _Realm.GameData.ActiveLanguage = Language.Japanese;
             
             var quests = _Realm.GameData.GetSheet<Quest>();
             var enpcs = _Realm.GameData.ENpcs;
             var objs = _Realm.GameData.GetSheet<EObj>();
-            var quest = quests[questId];
 
-            Console.WriteLine($"{quest.Id}: {quest.Name}");
+            var nextQuestsByQuest = new Dictionary<int, List<int>>();
+            foreach (var quest in quests)
+            {
+                var preReqQuests = quest.Requirements.PreviousQuest.PreviousQuests.Select(x => x.Key);
+                foreach (var preReq in preReqQuests)
+                {
+                    // TODO: a defaultdict would be nice here
+                    if (!nextQuestsByQuest.ContainsKey(preReq))
+                    {
+                        nextQuestsByQuest.Add(preReq, new List<int>());
+                    }
+                    nextQuestsByQuest[preReq].Add(quest.Key);
+                }
+            }
+
+            var result = new List<DSQ>();
+            
+            foreach (var quest in quests)
+            {
+                if (quest.Id.IsEmpty)
+                {
+                    continue;
+                }
+                _Realm.GameData.ActiveLanguage = Language.English;
+                Console.WriteLine($"{quest.Id}: {quest.Name}");
+                var q = ParseQuest(quest);
+                if (nextQuestsByQuest.TryGetValue(q.Key, out var nextQuests))
+                {
+                    q.NextQuests = nextQuests;
+                }
+
+                result.Add(q);
+            }
+
+            using var writer = File.CreateText(@"c:\temp\all-quest-texts.json");
+            JsonSerializer.Create(new JsonSerializerSettings { Formatting = Formatting.Indented })
+                .Serialize(writer, result);
+
+            return true;
+        }
+
+        private DSQ ParseQuest(Quest quest)
+        {
             var internalQuestIdNum = quest.Id.ToString().Split('_')[1];
 
-
             var result = new DSQ();
-            result.Key = questId;
+            result.Key = quest.Key;
             result.InternalId = quest.Id;
+            result.PreviousQuests = quest.Requirements.PreviousQuest.PreviousQuests.Select(x => x.Key).ToList();
 
             // TODO: this would be way better with valuetuples and destructuring, ie:
             // (result.TodosEn, result.JournalEn, result.DialogEn) = CollectTextData(...)
@@ -82,50 +120,7 @@ namespace SaintCoinach.Cmd.Commands {
             result.TodosJa = parsedJa.Item1;
             result.JournalJa = parsedJa.Item2;
             result.DialogueJa = parsedJa.Item3;
-            
-            Console.WriteLine(JsonConvert.SerializeObject(result, Formatting.Indented));
-
-            // foreach (var row in textData)
-            // {
-            //     var rowIdParts = ((XivString)row.GetRaw(0)).ToString().Split('_');
-            //     if (rowIdParts.Length != 6)
-            //     {
-            //         continue;
-            //     }
-            //     
-            //     Console.WriteLine($"{rowIdParts[3]}");
-            // }
-            
-            // for (int i = 0; i < 64; i++)
-            // {
-            //     var enpcId = quest.AsInt32("Listener", i);
-            //     if (enpcId == 0)
-            //     {
-            //         break;
-            //     }
-            //     Console.WriteLine(GetNpcOrObjectName(enpcs, objs, enpcId));
-            // }
-
-            // for (int i = 0; i < 50; i++)
-            // {
-            //     var scriptInstruction = quest.AsString("Script{Instruction}", i);
-            //     var scriptArg = quest.AsInt32("Script{Arg}", i);
-            //     var scripter = GetNpcOrObjectName(enpcs, objs, scriptArg);
-            //     Console.WriteLine($"{scripter}: {scriptInstruction}");
-            // }
-
-            // for (int i = 0; i < 32; i++)
-            // {
-            //     var uintA = quest.AsInt32("QuestUInt8A", i);
-            //     Console.WriteLine(uintA);
-            // }
-            // for (int i = 0; i < 32; i++)
-            // {
-            //     var uintB = quest.AsInt32("QuestUInt8B", i);
-            //     Console.WriteLine(uintB);
-            // }
-
-            return true;
+            return result;
         }
 
         private Tuple<List<string>,  List<string> , List<DialogLine> > CollectTextData(IXivSheet<XivRow> textData)
@@ -154,43 +149,37 @@ namespace SaintCoinach.Cmd.Commands {
                         continue; 
                     }
                     todos.Add(text);
-                } else if (rowIdParts.Length == 6 && rowIdParts[0] == "TEXT")
+                }
+                else if (rowIdParts.Length == 5 && rowIdParts[0] == "TEXT" && rowIdParts[3] == "POP" &&
+                         rowIdParts[4] == "MESSAGE")
+                {
+                    // skipping these for now
+                    continue;
+                }
+                else if (rowIdParts.Length == 5 && rowIdParts[0] == "TEXT" && rowIdParts[3] == "ACCESS" &&
+                         rowIdParts[4] == "MESSAGE")
+                {
+                    // skipping these for now
+                    continue;
+                }
+                else if (rowIdParts.Length == 7 && rowIdParts[0] == "TEXT" && (rowIdParts[4].StartsWith("Q") || rowIdParts[4].StartsWith("A")))
+                {
+                    // skipping these QAs for now
+                    continue;
+                }
+                else if (rowIdParts.Length == 6 && rowIdParts[0] == "TEXT")
                 {
                     var speaker = rowIdParts[3];
                     dialog.Add(new DialogLine { InternalId = rowId, Speaker = speaker, Text = text});
                 }
                 else
                 {
-                    throw new Exception($"Don't know how to handle text id {rowId}");
+                    // throw new Exception($"Don't know how to handle text id {rowId} with value {text}");
+                    Console.WriteLine($"Don't know how to handle text id {rowId} with value {text}");
                 }
             }
 
             return Tuple.Create(todos, journal, dialog);
-        }
-
-        private static string GetNpcOrObjectName(ENpcCollection enpcs, IXivSheet<EObj> objs, int objOrNpcId)
-        {
-            try
-            {
-                var enpc = enpcs[objOrNpcId];
-                return ($"{objOrNpcId}: {enpc.Singular}");
-            }
-            catch (Exception)
-            {
-            }
-
-            try
-            {
-
-                var obj = objs[objOrNpcId];
-                return ($"dummy {obj.Key}");
-            }
-            catch (Exception)
-            {
-                
-            }
-
-            return "unknown id " + objOrNpcId;
         }
     }
 }
