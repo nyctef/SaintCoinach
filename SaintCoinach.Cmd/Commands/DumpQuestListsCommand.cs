c68ebea3651de44870ee7ebb8d071dc914f4d39c
nyctef <nyctef@nyctef.com>
2023-04-28 21:39:13 +0100

    reorganising data for quest redo

--- SaintCoinach.Cmd/Commands/DumpQuestListsCommand.cs
+++ SaintCoinach.Cmd/Commands/DumpQuestListsCommand.cs
@@ -25,20 +25,71 @@ namespace SaintCoinach.Cmd.Commands {
             _Realm = realm;
         }
 
+        public struct RedoChapter
+        {
+            public string GroupEn;
+            public string GroupJa;
+            public string ChapterNameEn;
+            public string ChapterNameJa;
+            public RedoQuest[] Quests;
+        }
+
+        public struct RedoQuest
+        {
+            public int Key;
+            public string NameEn;
+            public string NameJa;
+        }
+
         public override async Task<bool> InvokeAsync(string paramList) {
 
             var questChain = _Realm.GameData.GetSheet<QuestRedo>();
 
+            var result = new List<RedoChapter>();
             foreach (var chain in questChain)
             {
-                Console.WriteLine(chain.Key);
-                Console.WriteLine(chain.FinalQuest?.Name ?? "<null>");
-                Console.WriteLine($" {chain.ChapterUI.UITab.Text}");
-                Console.WriteLine(
-                    $"{chain.ChapterUI.ChapterName} {chain.ChapterUI.ChapterPart} {chain.ChapterUI.Description}");
+                var rc = new RedoChapter();
+                _Realm.GameData.ActiveLanguage = Language.English;
+                rc.GroupEn = chain.ChapterUI.UITab.Text;
+                rc.ChapterNameEn = chain.ChapterUI.ChapterName;
+                _Realm.GameData.ActiveLanguage = Language.Japanese;
+                rc.GroupJa = chain.ChapterUI.UITab.Text;
+                rc.ChapterNameJa = chain.ChapterUI.ChapterName;
+
+                if (string.IsNullOrWhiteSpace(rc.GroupEn))
+                {
+                    // TODO: are these valid? or just incomplete?
+                    continue;
+                }
+
+                var quests = new List<RedoQuest>();
+
                 foreach (var quest in chain.Quests)
                 {
-                    Console.WriteLine($"  {quest.Key} {quest.Name}");
+                    _Realm.GameData.ActiveLanguage = Language.English;
+                    var nameEn = quest.Name;
+                    _Realm.GameData.ActiveLanguage = Language.Japanese;
+                    var nameJa = quest.Name;
+                    quests.Add(new RedoQuest { NameEn = nameEn, NameJa = nameJa, Key = quest.Key });
+                }
+
+                rc.Quests = quests.ToArray();
+                result.Add(rc);
+            }
+
+            var groups = result.GroupBy(x => x.GroupEn);
+            foreach (var group in groups)
+            {
+                Console.WriteLine($"{group.First().GroupEn} | {group.First().GroupJa}");
+                var redoChains = group.GroupBy(x => x.ChapterNameEn);
+                foreach (var redoChain in redoChains)
+                {
+                    Console.WriteLine($"  {redoChain.First().ChapterNameEn} | {redoChain.First().ChapterNameJa}");
+
+                    foreach (var q in redoChain.SelectMany(x => x.Quests))
+                    {
+                        Console.WriteLine($"      {q.NameEn}");
+                    }
                 }
             }
 
